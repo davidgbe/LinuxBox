1934b4eabe235a526de7a63f2d96ac1e2bd50ec4
Keshav Varma <keshavdv@gmail.com>
2014-10-04 16:33:13 -0700

    app management

---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,42 @@
+class AppsController < ApplicationController
+  def start
+    @instance = Instance.create
+  end
+
+  def index
+    @apps = App.where(user:current_user)
+  end
+
+  def new
+    @app =  App.new
+  end
+
+  def show
+    @app = App.find(params[:id])
+    if @app.user != current_user
+      head :forbidden and return
+    end
+  end
+
+  def create
+    @app = App.new(app_params)
+    @app.user = current_user
+    @app.save
+    render 'show'
+  end
+
+  def update
+  end
+
+  def destroy
+    @app = App.find(params[:id])
+    @app.destroy
+    head :ok and return
+  end
+
+  private
+
+  def app_params
+    params.require(:app).permit(:name)
+  end
+end
