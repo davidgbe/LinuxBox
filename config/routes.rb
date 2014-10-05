c8b5d60961a1efecae4dd6d8c3d7ea19df679bd0
David Bell <davidgbe@usc.edu>
2014-10-04 19:43:39 -0700

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   get 'instance/create'
   root 'info#index'
 
+  resouces :instances do 
+    collection do
+      post 'validate_vm'
+      get 'available'
+    end 
+  end
+
   resources :apps do
-    member do
+    collection do
       get 'start'
     end
   end
